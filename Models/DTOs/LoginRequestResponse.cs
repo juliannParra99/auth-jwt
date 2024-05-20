79d889913d33819a076902da087687080077a356
juliannparra99 <julianfranco.parra@gmail.com>
2024-05-20 14:08:22 -0300

    Added: 1.1 - added DTOs

---
+++ Models/DTOs/LoginRequestResponse.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Drivers.Api.Models.DTOs
+{
+    public class LoginRequestResponse : AuthResult
+    {
+        
+    }
+}
